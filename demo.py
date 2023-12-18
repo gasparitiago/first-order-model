8c50f00610b8cbcb03d33af076623b4d7f469a3e
Tiago De Gaspari <tiago.gaspari@ae.studio>
2023-12-18 17:15:51 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -64,18 +64,17 @@ def make_animation(source_image, driving_video, generator, kp_detector, relative
         driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
         kp_source = kp_detector(source)
         kp_driving_initial = kp_detector(driving[:, :, 0])
+        frame_idx = 5
+        driving_frame = driving[:, :, frame_idx]
+        kp_driving = kp_detector(driving_frame)
+        kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
+                                kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
+                                use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
 
         for frame_idx in tqdm(range(driving.shape[2])):
-            driving_frame = driving[:, :, frame_idx]
-            if not cpu:
-                driving_frame = driving_frame.cuda()
-            kp_driving = kp_detector(driving_frame)
-            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
-                                   kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
-                                   use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
             out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
-
             predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
+
     return predictions
 
 def find_best_frame(source, driving, cpu=False):
